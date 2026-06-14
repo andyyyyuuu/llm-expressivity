d571fc2f566c6cfe184fed14a573c149f866f159
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2026-06-14 14:54:22 -0700

    Adapt train.py to refactors

--- module.py
+++ module.py
@@ -12,11 +12,36 @@ class DownstreamModule(torch.nn.Module, ABC):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError("Forward pass not implemented")
     
+    @property
     @abstractmethod
+    def vocab_size(self) -> int:
+        raise NotImplementedError("Output distribution vocab size not implemented")
+    
+    @property
+    @abstractmethod
+    def device(self) -> int:
+        raise NotImplementedError("Device not implemented")
+    
+    @property
+    @abstractmethod
+    def input_shape(self) -> torch.Size:
+        raise NotImplementedError("Input shape not set")
+    
+    @abstractmethod
+    def __repr__(self) -> str:
+        raise NotImplementedError("__repr__ not implemented")
+    
+
     def validate_input(self, x: torch.Tensor) -> None:
-        raise NotImplementedError("Input validation not implemented")
+        """Generic validation for input tensor matching input_shape"""
+        if x.shape != self.input_shape:
+            raise ValueError(f"Input tensor must be of shape {self.input_shape}, got {x.shape}")
 
-
+    
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        self.validate_input(x)
+        return self.forward(x)
+    
 
 class LMIntervention(DownstreamModule, ABC):
     """Base class for interventions that need to load and trace model output"""
@@ -26,6 +51,17 @@ class LMIntervention(DownstreamModule, ABC):
         self.model = LanguageModel(model_name, device_map="auto", dispatch=True)
         self.prefix_length = prefix_length
     
+    @property
+    def vocab_size(self) -> int:
+        return self.model.config.vocab_size
+
+    @property
+    def device(self) -> torch.device:
+        return self.model.device
+
+    @property
+    def input_shape(self) -> torch.Size:
+        return (self.prefix_length, self.model.config.hidden_size)
 
     @abstractmethod
     def intervene(self, x: torch.Tensor) -> None:
@@ -33,19 +69,13 @@ class LMIntervention(DownstreamModule, ABC):
     
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        self.validate_input(x)
         dummy_input_ids = torch.zeros((1, self.prefix_length), dtype=torch.long, device=self.model.device)
         with self.model.trace(dummy_input_ids):
             self.intervene(x.unsqueeze(0))
             logits = self.model.output.save() # (1, L, V)
         final_logits = logits.logits.squeeze(0)[-1, :] # (V)
-        return final_logits
-    
-
-    def validate_input(self, x: torch.Tensor) -> None:
-        """Generic validation (L, H) for hidden state interventions and more"""
-        if x.shape != (self.prefix_length, self.model.config.hidden_size):
-            raise ValueError(f"Input tensor must be of shape (1, {self.prefix_length}, {self.model.config.hidden_size}), got {x.shape}")
+        log_probs = torch.log_softmax(final_logits, dim=-1)
+        return log_probs
 
 
 
@@ -60,6 +90,9 @@ class LayerIntervention(LMIntervention):
     
     def intervene(self, x: torch.Tensor) -> None:
         self.model.model.layers[self.layer].output[0] = x
+    
+    def __repr__(self) -> str:
+        return f"LayerIntervention(layer={self.layer})"
 
 
 
@@ -71,6 +104,9 @@ class EmbedIntervention(LMIntervention):
     
     def intervene(self, x: torch.Tensor) -> None:
         self.model.model.embed_tokens.output = x
+    
+    def __repr__(self) -> str:
+        return f"EmbedIntervention()"
 
 
 class PostNormIntervention(LMIntervention):
@@ -82,6 +118,8 @@ class PostNormIntervention(LMIntervention):
     def intervene(self, x: torch.Tensor) -> None:
         self.model.model.norm.output = x
     
+    def __repr__(self) -> str:
+        return f"PostNormIntervention()"
     
 
 
--- train.py
+++ train.py
@@ -3,8 +3,8 @@ from tqdm.auto import tqdm
 import torch
 from dotenv import load_dotenv
 import os
-from config import InterventionConfig
 from utils import set_seed
+from module import DownstreamModule, LayerIntervention
 
 load_dotenv()
 
@@ -15,38 +15,22 @@ if DO_WANDB:
     wandb.login(key=os.getenv("WANDB_API_KEY"))
 
 
-
-def intervene(model, patch: torch.Tensor, config: InterventionConfig) -> None: 
-    assert patch.shape == (1, config.prefix_length, model.config.hidden_size)
-    if config.type == "embed":
-        model.model.embed_tokens.output = patch
-    elif config.type == "layer":
-        model.model.layers[config.layer].output[0] = patch
-
-
-def tune_soft_prompt(model: LanguageModel, target_entropy: float, target_logits: torch.Tensor, config: InterventionConfig, lr: float=0.1, max_epochs: int=500, early_stop_patience: int=20, log_losses: bool=True) -> tuple[torch.nn.Parameter, float, bool]:
+def tune_soft_prompt(module: DownstreamModule, target_entropy: float, target_logits: torch.Tensor, lr: float=0.1, max_epochs: int=500, early_stop_patience: int=20, log_losses: bool=True) -> tuple[torch.nn.Parameter, float, bool]:
     
-    config.check_valid(model)
-
     if DO_WANDB:
         wandb.init(project="expressivity-of-llms-training", 
                    config={
                        "target_entropy": target_entropy,
-                       "prefix_length": config.prefix_length,
-                       "intervention_type": config.type,
-                       "intervention_layer": config.layer,
+                       "prefix_length": module.prefix_length,
+                       "intervention": module.__repr__(),
                        "lr": lr,
                    })
 
-    for param in model.model.parameters():
-        param.requires_grad = False
-    
-    model_dtype = model.model.embed_tokens.weight.dtype
     target_logits.requires_grad = False
     target_log_probs = torch.log_softmax(target_logits, dim=-1)
     
     soft_prompt = torch.nn.Parameter(
-        torch.randn(config.prefix_length, model.config.hidden_size, device=model.device, dtype=model_dtype),
+        torch.randn(module.input_shape, device=module.device, dtype=torch.float32),
         requires_grad=True,
     )
 
@@ -60,12 +44,7 @@ def tune_soft_prompt(model: LanguageModel, target_entropy: float, target_logits:
     early_stopped = False
 
     for epoch in tqdm(range(max_epochs), desc=f"training H={target_entropy:.2f}", leave=False):
-        input_ids = torch.zeros((1, config.prefix_length), dtype=torch.long, device=model.device)
-        with model.trace(input_ids):
-            intervene(model, soft_prompt.unsqueeze(0), config)
-            logits = model.output.save() # (1, L, V)
-        final_logits = logits.logits.squeeze(0)[-1, :] # (V)
-        log_probs = torch.log_softmax(final_logits, dim=-1)
+        log_probs = module.forward(soft_prompt)
         loss = loss_fn(log_probs.unsqueeze(0), target_log_probs.unsqueeze(0))
         optimizer.zero_grad()
         loss.backward()
@@ -95,14 +74,13 @@ if __name__ == "__main__":
     from dists import optimize_vanilla
 
     set_seed(int(os.getenv("TRAINING_SEED", "216")))
-    model = LanguageModel("meta-llama/Llama-3.2-1B", device_map="auto", dispatch=True)
+    module = LayerIntervention(layer=5, prefix_length=5)
     target_entropy = 0.5
-    prefix_length = 5
     lr = 0.1
     max_epochs = 500
     early_stop_patience = 20
-    target_logits = optimize_vanilla(target_entropy, model.config.vocab_size, 1e-4, device=model.device)
-    best_prompt, best_loss, early_stopped = tune_soft_prompt(model, target_entropy, target_logits, InterventionConfig(type="layer", layer=5, prefix_length=prefix_length), lr, max_epochs, early_stop_patience)
+    target_logits = optimize_vanilla(target_entropy, module.vocab_size, 1e-4, device=module.device)
+    best_prompt, best_loss, early_stopped = tune_soft_prompt(module, target_entropy, target_logits, lr, max_epochs, early_stop_patience)
     print(f"Best prompt: {best_prompt}")
     print(f"Best loss: {best_loss}")
     print(f"Early stopped: {early_stopped}")
